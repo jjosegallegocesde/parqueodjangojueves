417e75374b090171b97c86a5774a2605c92b48bd
Edward <asantanadurango@gmail.com>
2022-06-09 06:48:53 -0500

    se crea nuevo ambiente virtual

--- app/Scripts/activate.bat
+++ app/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\NT\nt-momento3\parqueodjangojueves\app
+set VIRTUAL_ENV=C:\Users\308\Documents\parqueodjangojueves\app
 
 if not defined PROMPT set PROMPT=$P$G
 
