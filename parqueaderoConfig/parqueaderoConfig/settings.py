6df02c84e1f3806fc860c834cadc740690b32a2a
Jhonatan Alexis Ossa Osorio - Ceiba Software <jhonatan.ossa@ceiba.com.co>
2022-06-09 08:46:49 -0500

    se migra la BD

--- parqueaderoConfig/parqueaderoConfig/settings.py
+++ parqueaderoConfig/parqueaderoConfig/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'parqueaderoConfig.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'parqueaderojueves',
+        'USER': 'root',
+        'PASSWORD': '',
     }
 }
 
